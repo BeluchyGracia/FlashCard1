da8fc57dc3ccc28376589b638ec7ecab6f206cc5
BeluchyGracia <beluchygracia64@gmail.com>
2024-03-13 13:14:19 -0700

    third

--- app/src/main/java/com/example/flashcard/MainActivity.kt
+++ app/src/main/java/com/example/flashcard/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.flashcard
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.RelativeLayout
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +12,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val flashcardQuestion = findViewById<TextView>(R.id.flashcard_question)
         val flashcardAnswer = findViewById<TextView>(R.id.flashcard_answer)
+        val textView1 = findViewById<TextView>(R.id.textView1)
+        val textView2 = findViewById<TextView>(R.id.textView2)
+        val textView3 = findViewById<TextView>(R.id.textView3)
+        val mainLayout = findViewById<RelativeLayout>(R.id.main_layout)
         flashcardAnswer.visibility = View.INVISIBLE
         flashcardQuestion.setOnClickListener {
             flashcardQuestion.visibility = View.INVISIBLE
@@ -19,6 +24,29 @@ class MainActivity : AppCompatActivity() {
         flashcardAnswer.setOnClickListener {
             flashcardAnswer.visibility = View.INVISIBLE
             flashcardQuestion.visibility = View.VISIBLE
+        }
+
+        textView1.setOnClickListener {
+            textView1.setBackgroundColor(resources.getColor(R.color.red))
+            textView3.setBackgroundColor(resources.getColor(R.color.green))
+        }
+
+        textView2.setOnClickListener {
+            textView2.setBackgroundColor(resources.getColor(R.color.red))
+            textView3.setBackgroundColor(resources.getColor(R.color.green))
+        }
+
+        textView3.setOnClickListener {
+            textView3.setBackgroundColor(resources.getColor(R.color.green))
+        }
+        mainLayout.setOnClickListener {
+            resetTextViewBackgrounds(textView1, textView2, textView3)
+        }
+    }
+
+    private fun resetTextViewBackgrounds(vararg textViews: TextView) {
+        for (textView in textViews) {
+            textView.setBackgroundColor(resources.getColor(R.color.beige_clair))
         }
     }
 }
